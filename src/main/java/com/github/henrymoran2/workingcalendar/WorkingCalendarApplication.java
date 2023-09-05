01849ad70c21f972dde1dba0ffe7d3a60fd7d781
HenryMorgan2 <taraskovdanil@gmail.com>
2023-09-05 11:57:00 +0300

    commit 1

---
+++ src/main/java/com/github/henrymoran2/workingcalendar/WorkingCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.github.henrymoran2.workingcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/henrymoran2/workingcalendar/WorkingCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.henrymoran2.workingcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
