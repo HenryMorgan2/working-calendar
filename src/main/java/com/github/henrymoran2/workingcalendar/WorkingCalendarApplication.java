84055df418434861d9c7412cb6e7d550e37d78e0
HenryMorgan2 <taraskovdanil@gmail.com>
2023-09-14 00:45:20 +0300

    AC: В корне репозитория должна появится директория с проектом working-calendar Собрать исполняемый jar и запустить его Необходимо сделать пул реквест

--- src/main/java/com/github/henrymoran2/workingcalendar/WorkingCalendarApplication.java
+++ src/main/java/com/github/henrymoran2/workingcalendar/WorkingCalendarApplication.java
@@ -8,6 +8,7 @@ public class WorkingCalendarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkingCalendarApplication.class, args);
+		System.out.println("Hello world!");
 	}
 
 }
